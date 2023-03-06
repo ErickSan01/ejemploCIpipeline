0f8a6dbed2a128b242d6e356d047567be02409c6
Erick <39204127@uaz.edu.mx>
2023-03-06 13:55:31 -0600

    Agrega funcion hola2

--- mensaje/views.py
+++ mensaje/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def hola(request):
     return render(request, 'hola.html')
+
+def hola2(request):
+    return render(request,'hola.html')
